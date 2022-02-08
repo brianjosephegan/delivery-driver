a115c461615fae3fa9e437ea8787e32a03239079
Brian Egan <brianjoseph.egan@gmail.com>
2022-02-07 21:48:14 -0800

    Assets
    Add project assets and adjust size.

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Ouch!");
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("What was that?!");
+    }
 }
